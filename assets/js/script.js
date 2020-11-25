a5a7e7faf3abbd5ddc9483ab34e65da1d775a340
Antoine Calegaro <antoine.calegaro@aeronomie.be>
2020-11-25 21:54:52 +0100

    testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 function main(dashboard){
     println(dashboard)
+    dashboard.tabs = {
+        "test" : {
+            "name" : "Test tab",
+            "navname" : "Tab navname"
+        }
+    }
+
+    parse(dashboard, 'body', dashboard.references)
 }
 
 function init(dashboard) {}
